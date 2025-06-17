0dc0a43914e9ab2fd6d76c01a784b5b26ee39c1a
HarshaMW1 <142015096+HarshaMW1@users.noreply.github.com>
2025-06-18 04:25:14 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import AddProduct from './pages/AddProduct';
+
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+      
+
+
+        
+
+        <Route path="/add-product" element={<AddProduct />} />
+       
+
+
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/AdminNavbar.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react';
+import './Navbar.css';
+import {
+  FaChartPie, FaSignOutAlt, FaBars,
+  FaClipboardList, FaFilePdf, FaPlusSquare, FaUsers
+} from 'react-icons/fa';
+import { useLocation, Link } from 'react-router-dom';
+
+function AdminNavbar({ onLogoutClick }) {
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const location = useLocation();
+
+  const isActive = (path) => location.pathname === path;
+
+  const navItems = [
+    
+    { path: '/add-product', label: 'Products', icon: <FaPlusSquare /> },
+    
+  ];
+
+  return (
+    <>
+      <nav className="navbar">
+        <img src="/Images/logo.png" alt="logo" className="logo" />
+        <div className="nav-links">
+          {navItems.map(item => (
+            <Link
+              key={item.path}
+              to={item.path}
+              className={`nav-btn ${isActive(item.path) ? 'active' : ''}`}
+            >
+              {item.icon} {item.label}
+            </Link>
+          ))}
+          {/*<span onClick={onLogoutClick} className="nav-btn logout-btn">
+            <FaSignOutAlt /> Logout
+          </span> */}
+        </div>
+        <div className="hamburger" onClick={() => setShowMobileMenu(!showMobileMenu)}>
+          <FaBars />
+        </div>
+      </nav>
+
+      {showMobileMenu && (
+        <div className="mobile-menu">
+          {navItems.map(item => (
+            <Link
+              key={item.path}
+              to={item.path}
+              className={`nav-btn ${isActive(item.path) ? 'active' : ''}`}
+              onClick={() => setShowMobileMenu(false)}
+            >
+              {item.icon} {item.label}
+            </Link>
+          ))}{/*
+          <span onClick={onLogoutClick} className="nav-btn logout-btn">
+            <FaSignOutAlt /> Logout
+          </span> */}
+        </div>
+      )}
+    </>
+  );
+}
+
+export default AdminNavbar;
